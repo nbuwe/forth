cbd40b7ed62581dff6585985373ced758f93a311
"Valery Ushakov" <uwe@stderr.spb.ru>
2009-01-31 21:04:27 +0300

    Initial revision

---
+++ Makefile
@@ -0,0 +1,9 @@
+# $Id$
+
+PROG=	ftest
+NOMAN=
+
+DBG=	-g
+SRCS=	main.c forth.S
+
+.include <bsd.prog.mk>
---
+++ main.c
@@ -0,0 +1,40 @@
+/* $Id$ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+extern void *test(int);
+extern int32_t stack_bottom[];
+
+int
+main()
+{
+    int32_t *psp;
+    int32_t *p;
+
+    psp = test(10);
+    for (p = psp; p < stack_bottom; ++p) {
+	printf("0x%08x  %d\n", *p, *p);
+    }
+    
+    return 0;
+}
+
+
+void
+emit_impl(int32_t character)
+{
+    char c = (char)character;
+    fputc(c, stdout);
+    fflush(stdout);
+    return;
+}
+
+
+void
+type_impl(const char *data, int32_t size)
+{
+    fwrite(data, size, 1, stdout);
+    fflush(stdout);
+    return;
+}
